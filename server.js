d1dcd38f96d4afca84fe449d7fbebc8b88189df1
Arman Yerkesh <a.yerkesh@gmail.com>
2021-10-15 08:52:40 +0300

    Add live reload server in dev environment

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 const express = require("express");
 const app = express();
+app.set('view engine', 'ejs');
 
 if (process.env.NODE_ENV !== 'production') {
+  const livereload = require("livereload");
+  const conLivereload = require("connect-livereload");
+
   require("dotenv").config();
+
+  const skey = process.env.STRIPE_SKEY;
+  const pkey = process.env.STRIPE_PKEY;
+
+  const liveServer = livereload.createServer();
+  liveServer.watch('public');
+
+  app.use(conLivereload());
+
+  liveServer.server.once('connection', () => {
+    setTimeout(() => {
+      liveServer.refresh('/');
+    }, 1000);
+  })
 }
 
-const skey = process.env.STRIPE_SKEY;
-const pkey = process.env.STRIPE_PKEY;
-
-app.set('view engine', 'ejs');
 app.use(express.static('public'));
-
 app.listen(3000);
