c30cc03d15fefbbc563da44e323e6b60cb878e1e
Arman Yerkesh <a.yerkesh@gmail.com>
2021-10-15 08:23:28 +0300

    Import Stripe API keys from .env

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
 
-app.set('view engine', ejs);
+if (process.env.NODE_ENV !== 'production') {
+  require("dotenv").config();
+}
+
+const skey = process.env.STRIPE_SKEY;
+const pkey = process.env.STRIPE_PKEY;
+
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.listen(3000);
