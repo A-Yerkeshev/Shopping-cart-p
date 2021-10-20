188d2e25119f10a12f6a7c51f95c9760dd5a6b33
Arman Yerkesh <a.yerkesh@gmail.com>
2021-10-20 12:20:52 +0300

    Connect to database using different method when on production

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const { Client } = require("pg");
+const client;
 
 const app = express();
 
 if (process.env.NODE_ENV !== 'production') {
   // Require dotenv and livereload
-  // Create live reload server and connect to app
   const livereload = require("livereload");
   const conLivereload = require("connect-livereload");
-
   require("dotenv").config();
 
+  // Create live reload server and connect to app
   const liveServer = livereload.createServer();
   liveServer.watch('public');
 
@@ -21,30 +21,39 @@ if (process.env.NODE_ENV !== 'production') {
       liveServer.refresh('/');
     }, 1000);
   })
-}
 
-const skey = process.env.STRIPE_SKEY;
-const pkey = process.env.STRIPE_PKEY;
-
-// app.set('view engine', 'ejs');
-
-app.use('/', express.static('public'));
-
-app.get('/products', (request, response) => {
-  const client = new Client({
+  // Connect to database using .env variables
+  client = new Client({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     database: process.env.DB,
     password: process.env.DB_PASSWORD
   })
-
-  client.connect((error) => {
-    if (error) {console.log('Database connection error.', error.stack)} else {
-      console.log('Connection to database established');
+} else {
+  // Connect to Heroku database
+  client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
     }
-  });
+  })
+}
 
+const skey = process.env.STRIPE_SKEY;
+const pkey = process.env.STRIPE_PKEY;
+
+client.connect((error) => {
+  if (error) {console.log('Database connection error.', error.stack)} else {
+    console.log('Connection to database established');
+  }
+})
+
+// app.set('view engine', 'ejs');
+
+app.use('/', express.static('public'));
+
+app.get('/products', (request, response) => {
   client.query('SELECT * from products', (error, data) => {
     if (error) {console.log(error.stack)}
     response.status(200).send(data.rows);
