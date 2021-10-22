b39abbc5c40bb5d8d30d17d2b189df49c90e2984
Arman Yerkesh <a.yerkesh@gmail.com>
2021-10-22 09:10:09 +0300

    Make fillTemplate function do a shallow filling

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,46 @@
+"use strict";
+
 const main = document.getElementsByTagName('main')[0];
+const template = document.getElementById('item-template');
+
+function fillTemplate(template, data) {
+  // Check if template is a node element
+  if (!template || template.nodeType !== Node.ELEMENT_NODE) {
+    throw new Error('First argument passed to "fillTemplate" function must be a node element.');
+    return;
+  }
+  // Check if data is object
+  if (typeof data !== 'object') {
+    throw new Error('Second argument passed to "fillTemplate" function must be an object.');
+    return;
+  }
+
+  let string = template.innerHTML;
+  let start = string.indexOf('{{');
+  let end = string.indexOf('}}');
+
+  while(start >= 0 && end >= 0) {
+    const varname = string.substring(start+2, end).trim();
+
+    if (data[varname]) {
+      string = string.substring(0, start) + String(data[varname]) + string.substring(end+2);
+
+      start = string.indexOf('{{');
+      end = string.indexOf('}}');
+    } else {
+      throw new Error(`Cannot fill template. Variable ${varname} is not defined.`);
+      return;
+    }
+  }
+
+  return string;
+}
 
 fetch('/products')
   .then(response => response.json())
   .then((data) => {
-    const pre = document.createElement('pre');
-    pre.innerText = JSON.stringify(data, null, 2);
-    main.appendChild(pre);
+    console.log(fillTemplate(template, data[0]));
+  }).catch((error) => {
+    console.log('Error fetching data from /products.', error);
   })
+
--- server.js
+++ server.js
@@ -61,3 +61,5 @@ app.get('/products', (request, response) => {
 })
 
 app.listen(process.env.PORT || 3000);
+
+console.log('Server is running');
