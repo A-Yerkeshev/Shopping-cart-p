96279d93e95775ffc9c5cad2edd19345935bd36c
Arman Yerkesh <a.yerkesh@gmail.com>
2021-10-22 11:32:01 +0300

    Make fillTemplate function support <repeat> tags

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,9 @@
 "use strict";
 
+const log = console.log;
 const main = document.getElementsByTagName('main')[0];
-const template = document.getElementById('item-template');
+// const template = document.getElementById('item-template');
+const template = document.getElementById('store');
 
 function fillTemplate(template, data) {
   // Check if template is a node element
@@ -15,11 +17,62 @@ function fillTemplate(template, data) {
     return;
   }
 
+  // Check for repeat statements
+  const repeatTags = template.content.querySelectorAll('repeat');
+
+  for (let repeat of repeatTags) {
+    // Find iterated array
+    const attr = repeat.getAttribute('for');
+    if (!attr) {
+      throw new Error(`<repeat> tag expects "for" attribute.`);
+      return;
+    }
+
+    const ofI = attr.indexOf(' of ');
+    if (ofI == -1) {
+      throw new Error(`<repeat> tag's "for" attribute must have following syntax: for="/value/ of /iterable/".`);
+      return;
+    }
+
+    const iterName = attr.substring(ofI+4).trim();
+    const iterable = data[iterName];
+    if (!iterable) {
+      throw new Error(`Iterable "${iterName}" is not defined.`);
+      return;
+    } else if (!Array.isArray(iterable) && !(iterable instanceof Set)) {
+      throw new Error(`Iterable value "${iterName}" specified in "for=" attribute of the <repeat> tag must be an array or set.`);
+      return;
+    }
+
+    // By this point iterable is found and has correct type
+
+    // Iterate through iterable, fill new template on every iteration, append result to outputStr
+    let outputStr = '';
+
+    iterable.forEach((element) => {
+      const template = document.createElement('template');
+      const content = repeat.childNodes;
+
+      for (let node of content) {
+        template.content.append(node.cloneNode(true));
+      }
+
+      outputStr += fillTemplate(template, element);
+    })
+
+    // Replace <repeat> tag with actual content
+    const parsed = document.createRange().createContextualFragment(outputStr);
+
+    repeat.parentNode.insertBefore(parsed, repeat);
+    repeat.remove();
+  }
+
+  // Replace template variables with values
   let string = template.innerHTML;
   let start = string.indexOf('{{');
   let end = string.indexOf('}}');
 
-  while(start >= 0 && end >= 0) {
+  while (start >= 0 && end >= 0) {
     const varname = string.substring(start+2, end).trim();
 
     if (data[varname]) {
@@ -28,7 +81,7 @@ function fillTemplate(template, data) {
       start = string.indexOf('{{');
       end = string.indexOf('}}');
     } else {
-      throw new Error(`Cannot fill template. Variable ${varname} is not defined.`);
+      throw new Error(`Cannot fill template. Variable "${varname}" is not defined.`);
       return;
     }
   }
@@ -36,11 +89,14 @@ function fillTemplate(template, data) {
   return string;
 }
 
-fetch('/products')
-  .then(response => response.json())
-  .then((data) => {
-    console.log(fillTemplate(template, data[0]));
-  }).catch((error) => {
-    console.log('Error fetching data from /products.', error);
-  })
+// fetch('/products')
+//   .then(response => response.json())
+//   .then((data) => {
+//     console.log(fillTemplate(template, data[0]));
+//   }).catch((error) => {
+//     console.log('Error fetching data from /products.', error);
+//   })
 
+const p = document.createElement('p');
+p.textContent = fillTemplate(template, {items: [{}, {}, {}]});
+main.appendChild(p);
